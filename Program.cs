697563ed22553d486c5dea13d038f27517b61d9d
Ed Sadler <ed_lorie@comcast.net>
2017-07-12 07:24:50 -0500

    complete

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-namespace dictionaries
+    namespace dictionaries
 {
     class Program
     {
@@ -13,29 +13,57 @@ namespace dictionaries
             stocks.Add("LIL", "Lil Cakes");
             stocks.Add("ME", "My Entertainment");
 
-            string GM = stocks["GM"];   <--- "General Motors"
+            Console.WriteLine(stocks["GM"]);
+            Console.WriteLine(stocks["CAT"]);
+            Console.WriteLine(stocks["LIL"]);
+            Console.WriteLine(stocks["ME"]);
 
             List<(string ticker, int shares, double price)> purchases = new List<(string, int, double)>();
 
-            purchases.Add((ticker: "GE", shares: 150, price: 23.21));
-            purchases.Add((ticker: "GE", shares: 32, price: 17.87));
-            purchases.Add((ticker: "GE", shares: 80, price: 19.02));
+            purchases.Add((ticker: "GM", shares: 150, price: 23.21));
+            purchases.Add((ticker: "GM", shares: 32, price: 17.87));
+            purchases.Add((ticker: "GM", shares: 80, price: 19.02));
+
+            purchases.Add((ticker: "CAT", shares: 20, price: 22.05));
+            purchases.Add((ticker: "CAT", shares: 40, price: 18.00));
+            purchases.Add((ticker: "CAT", shares: 60, price: 16.00));
+
             purchases.Add((ticker: "LIL", shares: 10, price: 15.50));
             purchases.Add((ticker: "LIL", shares: 20, price: 16.00));
+
             purchases.Add((ticker: "ME", shares: 25, price: 17.05));
             purchases.Add((ticker: "ME", shares: 30, price: 18.05));
 
-            // Iterate over the purchases and 
+            Dictionary<string, int>allStocks = new Dictionary<string, int>();
+
             foreach ((string ticker, int shares, double price) purchase in purchases)
             {
-                // Does the company name key already exist in the report dictionary?
 
-                // If it does, update the total valuation
+            if (stocks.ContainsKey (purchase.ticker)){
 
-                // If not, add the new key and set its value
+                if (allStocks.ContainsKey (stocks[purchase.ticker])){
+                    allStocks[stocks[purchase.ticker]] += (int) purchase.price * (int)purchase.shares;
+
+                } else {
+                    
+
+                        allStocks.Add(stocks[purchase.ticker], (int) purchase.price * (int) purchase.shares);
+                    
+                    }
+            }
             }
 
-            Console.WriteLine("Hello World!");
+            foreach (KeyValuePair<string, int> purchase in allStocks)
+            {
+        
+            
+                Console.WriteLine ($"{purchase.Key} sold {purchase.Value}");
+
+            }
         }
     }
 }
+
+            
+            
+        
